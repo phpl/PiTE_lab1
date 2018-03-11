b582f863ac7d699471c495fe998c89fa0951b716
Paweł Hadam <pawel.hadam19@gmail.com>
2018-03-11 19:53:59 +0100

    Add main script

--- Main.py
+++ Main.py
@@ -1,2 +1,14 @@
+from src.logic.ArgParser import ArgParser
+from src.logic.FileCounter import FileCounter
+from src.validators.FileValidator import FileValidator
+from src.validators.FlagsValidator import FlagsValidator
+
 if __name__ == '__main__':
-    pass
+    parser = ArgParser()
+    arguments = parser.parse_arguments_to_dictionary()
+
+    FileValidator.validate(arguments['filename'])
+    FlagsValidator.validate(arguments['flag'])
+
+    file_counter = FileCounter()
+    file_counter.count(**arguments)
